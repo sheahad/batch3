c30170379e6fd2311c92ceaab4bb45d3a69d29c5
sheahad <sheahad@gmail.com>
2019-05-21 10:02:12 +0600

    MessageShowed

--- MyWinApp/MyWinApp/HomeForm.Designer.cs
+++ MyWinApp/MyWinApp/HomeForm.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ShowButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(552, 69);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 0;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShowButton);
             this.Name = "HomeForm";
             this.Text = "Home";
             this.ResumeLayout(false);
@@ -42,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ShowButton;
     }
 }
 
--- MyWinApp/MyWinApp/HomeForm.cs
+++ MyWinApp/MyWinApp/HomeForm.cs
@@ -16,5 +16,10 @@ namespace MyWinApp
         {
             InitializeComponent();
         }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello..........");
+        }
     }
 }
